4879aeffbe75e5ca71d83b66e5611f7fb10ce438
Anthony3Dimensions <toniaguilarsanz2002@gmail.com>
2024-02-23 13:53:31 +0100

    VideojuegoOptimizacion

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float bulletSpeed = 5;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * bulletSpeed * Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+       gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    [SerializeField] Transform gunPosition;
+    [SerializeField] int bulletType = 0;
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Translate(movement*speed*Time.deltaTime);
+
+      
+        Shoot();
+        
+
+    }
+    void Shoot()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject bullet = PoolManager2.Instance.GetPooledObjects(bulletType, gunPosition.position, gunPosition.rotation);
+
+            if(bullet != null)
+            {
+                bullet.SetActive(true);
+            }
+            else
+            {
+                Debug.LogError("Pool demasiado pequeño");
+            }
+        }
+    }
+
+}
